a8f26f3f43c0a9a7ad091e9aae7d9499d9f625ba
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-15 12:00:35 +0530

    Updating data function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import supabase from "./supabase-client";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [toDoList, setToDoList] = useState([]);
   const [newToDo, setNewToDo] = useState("");
 
+  useEffect(() => {
+    fetchToDos();
+  }, []);
+
+  //adding ToDo's
   const addToDo = async () => {
     const newToDoData = {
       Name: newToDo,
@@ -15,10 +20,32 @@ function App() {
       .from("TodoList")
       .insert([newToDoData])
       .single();
-    if(error) console.log("Error adding todo", error);
-    else{
+    if (error) console.log("Error adding todo", error);
+    else {
       setToDoList((prev) => [...prev, data]);
       setNewToDo("");
+    }
+  };
+
+  //fetching ToDo's
+  const fetchToDos = async () => {
+    const { data, error } = await supabase.from("TodoList").select("*");
+    if (error) console.log("Error fetching Data", error);
+    else {
+      setToDoList(data);
+    }
+  };
+
+  //Updating ToDo's
+  const completeTask = async (id, isCompleted) => {
+    const { data, error } = await supabase
+      .from("TodoList")
+      .update({ isCompleted: !isCompleted })
+      .eq("id", id);
+
+    if(error) console.log("Error updating the data");
+    else{
+      fetchToDos();
     }
   };
 
@@ -29,11 +56,22 @@ function App() {
         <input
           type="text"
           placeholder="New ToDo...."
+          value={newToDo}
           onChange={(e) => setNewToDo(e.target.value)}
         />
         <button onClick={addToDo}>Add ToDo Item</button>
       </div>
-      <ul></ul>
+      <ul>
+        {toDoList.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.Name}</span>
+            <button onClick={() => completeTask(toDo.id, toDo.isCompleted)}>
+              {toDo.isCompleted ? "Undo" : "Complete task"}
+            </button>
+            <button>Delete Task</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
